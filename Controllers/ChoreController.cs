56e8b2c21eedb9d992fc17b5e8c0b61c0bda85e7
Josh Rittenberry <141363044+JoshRittenberry@users.noreply.github.com>
2024-01-17 14:27:48 -0600

    Setup for ChoreController Complete

---
+++ Controllers/ChoreController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using HouseRules.Data;
+using HouseRules.Models.DTOs;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using HouseRules.Models;
+using HouseRules.ModelsDTOs;
+
+namespace HouseRules.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ChoreController : ControllerBase
+{
+    private HouseRulesDbContext _dbContext;
+
+    public ChoreController(HouseRulesDbContext context)
+    {
+        _dbContext = context;
+    }
+}
